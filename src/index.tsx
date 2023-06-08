a529877e90100390e0bd381844b1956e266c95f7
Abhishek Batra <abhishek.batra@infobeans.com>
2023-06-08 18:19:35 +0530

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -41,15 +41,7 @@ const SDK = (props) => {
       })
       .then((response) => {
         // handle success
-        console.log('response from SDK++++', response);
-        console.log('response.data.ads_data', response.data.ads_data);
-        console.log('response.data.ads_data[0]', response.data.ads_data[0].ads);
-        console.log(
-          'response.data.ads_data[0].ads[0].media_details',
-          response.data.ads_data[0].ads[0].media_details
-        );
-
-        setVideo(response.data.ads_data[0].ads[0].media_details);
+        setVideo(response.data.data.ads_data[0].media_details);
       })
       .catch(function (error) {
         // handle error
